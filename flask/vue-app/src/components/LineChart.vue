23e2afe679c0dbc98c89ccdb2abb27201b1c237b
nobuyuki furukawa <t_furu@tf-web.jp>
2020-12-15 17:46:18 +0900

    add LineChart.vue

---
+++ flask/vue-app/src/components/LineChart.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { Component, Prop, Mixins } from 'vue-property-decorator';
+
+import Chart from "chart.js";
+import { Line, Bar, mixins } from "vue-chartjs";
+
+@Component({
+  components: {
+    Line
+  }
+})
+export default class LineChart extends Mixins(Line, Bar, mixins.reactiveProp) {
+  @Prop() public chartData!: Chart.ChartData;
+  @Prop() public chartOptions!: Chart.ChartOptions;
+
+  public mounted() {
+    console.log(this);
+    this.renderChart(this.chartData!, this.chartOptions!);
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- flask/vue-app/src/views/Home.vue
+++ flask/vue-app/src/views/Home.vue
@@ -1,18 +1,175 @@
 <template>
   <div class="home">
+    <!--
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    -->
+    <LineChart :chartData="chartData" :chartOptions="chartOptions" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+import Chart from "chart.js";
+import LineChart from '@/components/LineChart.vue';
+
+const FONT_COLOR = "rgba(255, 255, 255, 1)";
+const GRID_LINES_SETTING = {
+  display: true,
+  drawOnChartArea: false,
+  color: "rgba(255, 255, 255, .5)",
+  zeroLineColor: "rgba(255, 255, 255, 1)"
+};
 
 @Component({
   components: {
-    HelloWorld,
+    LineChart,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  chartData: Chart.ChartData = {};
+  chartOptions: Chart.ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      // display: false
+      onClick(event, legendItem) {
+        return;
+      },
+      fullWidth: false,
+      labels: {
+        boxWidth: 20,
+        fontColor: FONT_COLOR
+      },
+    },
+    layout: {
+      padding: {
+        top: 20,
+        left: 20,
+        bottom: 20,
+        right: 20
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: GRID_LINES_SETTING,
+          ticks: {
+            autoSkip: true,
+            fontColor: FONT_COLOR,
+            fontSize: 14
+          },
+          scaleLabel: {
+            display: true,
+            fontColor: FONT_COLOR,
+            labelString: "月",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          id: "yAxis_1",
+          type: "linear",
+          gridLines: GRID_LINES_SETTING,
+          scaleLabel: {
+            display: true,
+            fontColor: FONT_COLOR,
+            labelString: "湿度(%)"
+          },
+          ticks: {
+            autoSkip: true,
+            fontColor: FONT_COLOR,
+            fontSize: 14,
+            min: 0,
+            max: 100
+          },
+        },
+        {
+          id: "yAxis_2",
+          type: "linear",
+          gridLines: GRID_LINES_SETTING,
+          scaleLabel: {
+            display: true,
+            fontColor: FONT_COLOR,
+            labelString: "温度(℃)"
+          },
+          ticks: {
+            autoSkip: true,
+            fontColor: FONT_COLOR,
+            fontSize: 14,
+            min: 0,
+            max: 40
+          },
+          position: "right"
+        },
+        {
+          id: "yAxis_3",
+          type: "linear",
+          gridLines: GRID_LINES_SETTING,
+          scaleLabel: {
+            display: true,
+            fontColor: FONT_COLOR,
+            labelString: "C02"
+          },
+          ticks: {
+            autoSkip: true,
+            fontColor: FONT_COLOR,
+            fontSize: 14,
+            min: 0,
+            max: 40
+          },
+          position: "right"
+        }        
+      ],
+    }    
+  };
+
+  public created() {
+    this.createChartData();
+  }
+
+public createChartData() {
+    this.chartData = {
+      labels: ["1", "2", "3", "4", "5"],
+      datasets: [
+        {
+          yAxisID: "yAxis_1",
+          type: "Bar",
+          label: "湿度",
+          backgroundColor: "#6090EF",
+          borderColor: "#6090EF",
+          fill: false,
+          data: this.craeteRamdomValue(100)
+        },
+        {
+          yAxisID: "yAxis_2",
+          type: "line",
+          label: "温度",
+          backgroundColor: "#F87979",
+          borderColor: "#F87979",
+          fill: false,
+          data: this.craeteRamdomValue(40)
+        },
+        {
+          yAxisID: "yAxis_3",
+          type: "line",
+          label: "C02",
+          backgroundColor: "#607979",
+          borderColor: "#607979",
+          fill: false,
+          data: this.craeteRamdomValue(40)
+        }        
+      ]
+    };
+  }
+
+  public craeteRamdomValue(baseNumber: number) {
+    const arr: number[] = [];
+    for (let i = 0; i < 5; i++) {
+      arr.push(Math.floor(Math.random() * baseNumber));
+    }
+    return arr;
+  }
+}
 </script>
